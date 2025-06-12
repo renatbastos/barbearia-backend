204ed8349b7754b13f5160d0402342fece1fb7f0
Renato <renatobastos05@gmail.com>
2025-06-11 21:37:18 -0300

    Nova configuração de fuso-horário

--- src/main/java/com/barbearia/agenda/AgendaApplication.java
+++ src/main/java/com/barbearia/agenda/AgendaApplication.java
@@ -1,7 +1,11 @@
 package com.barbearia.agenda;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class AgendaApplication {
@@ -9,4 +13,10 @@ public class AgendaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApplication.class, args);
 	}
+
+	
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 }
