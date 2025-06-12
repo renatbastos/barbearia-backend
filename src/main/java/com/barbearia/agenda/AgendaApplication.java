16eafba853166a8ef199343436ac8f36ecce95a5
Renato <renatobastos05@gmail.com>
2025-06-11 21:45:36 -0300

    Alteração na classe principal

--- src/main/java/com/barbearia/agenda/AgendaApplication.java
+++ src/main/java/com/barbearia/agenda/AgendaApplication.java
@@ -13,10 +13,4 @@ public class AgendaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApplication.class, args);
 	}
-
-	
-    @PostConstruct
-    public void init(){
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
-    }
 }
