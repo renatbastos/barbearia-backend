e67498d04bde290e6893ca0e36506eca08db4d78
renatbastos <107721623+renatbastos@users.noreply.github.com>
2025-07-13 16:03:08 -0300

    Update AgendamentoController.java

--- src/main/java/com/barbearia/agenda/controller/AgendamentoController.java
+++ src/main/java/com/barbearia/agenda/controller/AgendamentoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://renatbastos.github.io")
 @RestController
 @RequestMapping("/agendamentos")
 public class AgendamentoController {
